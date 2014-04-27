598991e2bcbe5a933d9b22eb7ae67d3a9ead9c3b
sibi <sibi@sibi-PC>
2014-04-27 21:50:21 +0300

    tema duzenlemesi yapildi.

--- abacus/src/main/resources/scripts/import.sql
+++ abacus/src/main/resources/scripts/import.sql
@@ -2,7 +2,7 @@ INSERT INTO company (id, name, level, parent_id) VALUES ('01', 'HOLDING', 1, nul
 INSERT INTO company (id, name, level, parent_id) VALUES ('01.01', 'SIRKET', 2, '01');
 INSERT INTO company (id, name, level, parent_id) VALUES ('01.01.01', 'PROJE', 3, '01.01');
 
-INSERT INTO sec_authority (id) VALUES ('KULLANICI_ISLEMLER');
+INSERT INTO sec_authority (id) VALUES ('KULLANICI_ISLEMLERI');
 INSERT INTO sec_authority (id) VALUES ('KULLANICI_GRUP_ISLEMLERI');
 
 INSERT INTO sec_user( id, is_active, password, company_id) VALUES ('admin', 1, 'e10adc3949ba59abbe56e057f20f883e', '01');

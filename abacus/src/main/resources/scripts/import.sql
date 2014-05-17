109fd50a5673217b05d063ef2858d6e5bb01a639
asimerkut <asimerkut@gmail.com>
2014-05-17 17:18:19 +0300

    sa

--- abacus/src/main/resources/scripts/import.sql
+++ abacus/src/main/resources/scripts/import.sql
@@ -63,8 +63,8 @@ insert into def_task (id, organization_id, type_id, code, name, is_active, versi
 insert into def_task (id, organization_id, type_id, code, name, is_active, version) values (nextval('seq_id'), '#', 'STK_IO_O', '1', 'Stk Cikis #', 1, 0);
 
 insert into def_unit_group (id, organization_id, code, name, version) values (0, '#', '*', 'Bilinmeyen', 0);
-insert into def_unit_group (id, organization_id, code, name, version) values (nextval('seq_id'), '#', 'S', 'Yemek', 0);
-insert into def_unit_code (id, unit_group_id, code, name, ratio, version) values (nextval('seq_id'), currval('seq_id')-1, 'P', 'Porsiyon', 1, 0);
+insert into def_unit_group (id, organization_id, code, name, version) values (nextval('seq_id'), '#', 'Y', 'Yemek', 0);
+insert into def_unit_code (id, unit_group_id, code, name, ratio, version) values (nextval('seq_id'), currval('seq_id')-1, 'PR', 'Porsiyon', 1, 0);
 insert into def_unit_group (id, organization_id, code, name, version) values (nextval('seq_id'), '#', 'S', 'Sayilabilen', 0);
 insert into def_unit_code (id, unit_group_id, code, name, ratio, version) values (nextval('seq_id'), currval('seq_id')-1, 'AD', 'Adet', 1, 0);
 insert into def_unit_code (id, unit_group_id, code, name, ratio, version) values (nextval('seq_id'), currval('seq_id')-2, 'DS', 'Deste', 10, 0);

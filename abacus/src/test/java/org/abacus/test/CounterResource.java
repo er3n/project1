5d28d22ad08a48662efa6e8d415087f70c7d57ae
er3n <erenozturk@gmail.com>
2014-07-12 18:50:21 +0300

    ddsdcdc

---
+++ abacus/src/test/java/org/abacus/test/CounterResource.java
@@ -0,0 +1,14 @@
+package org.abacus.test;
+
+import org.primefaces.push.annotation.OnMessage;
+import org.primefaces.push.annotation.PushEndpoint;
+import org.primefaces.push.impl.JSONEncoder;
+
+@PushEndpoint("/counter")
+public class CounterResource {
+ 
+    @OnMessage(encoders = {JSONEncoder.class})
+    public String onMessage(String count) {
+        return count;
+    }
+}
